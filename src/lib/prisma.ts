1e9f979ff6ebe0ec2b6606b41c0a5b8aba410109
andrewkannan <kannanandrew101@gmail.com>
2026-05-20 15:09:52 +0800

    Update Prisma to v7.8.0 and fix log spam

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/generated/client/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL || "";
+const adapter = new PrismaPg({
+  connectionString,
+});
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query"],
+    adapter,
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
