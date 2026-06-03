0f319111b22deccdec672b540b349fb0c7c900ae
andrewkannan <kannanandrew101@gmail.com>
2026-06-03 16:51:08 +0800

    Optimize database by switching to lightning-fast native Prisma Rust engine

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,24 +1,11 @@
 import { PrismaClient } from "../../prisma/generated/client/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = process.env.DATABASE_URL || "";
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient, adapter: PrismaPg };
-
-if (!globalForPrisma.adapter) {
-  globalForPrisma.adapter = new PrismaPg({
-    connectionString,
-    max: 15,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 5000,
-  });
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter: globalForPrisma.adapter,
-  });
+  // @ts-ignore
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
